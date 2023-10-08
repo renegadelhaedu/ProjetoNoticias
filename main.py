1de272b190bf500ddee015040bc6f5f5ed09b472
feelipefst <feelipefst@gmail.com>
2023-10-08 13:44:20 -0300

    menu completo

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-login = int('00')
+menu = int('00')
 user = []
 adm = []
 
-print('Escolha a opção desejada: \n\n1 - Cadastrar Usuário \n2 - Login \n0 - Sair')
+while (menu != '0'):
+    print('1 - Cadastrar Usuário')
+    print('2 - Login')
+    print('0 - Sair')
+    menu = int(input('Escolha a opção desejada: '))
+
+    if (menu == 1):
+        print('\nNessa tela voce realizará seu cadastro')
+
+    elif (menu == 2):
+        print('\nInforme seu usuário e senha')
+
+    elif (menu == 99):
+        print('\nCrie seu usário SuperAdmin\n')
+
+    elif (menu == 0):
+        print('\nObrigado por usar nosso sistema!')
+        break
+
+    else:
+        print('Opção Invalida. Tente novamente')
 
